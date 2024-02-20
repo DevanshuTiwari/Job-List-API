48c3fb7e5c8d5498760ef185710c4d7f000da7fe
Devanshu Tiwari <devanshu1336@gmail.com>
2024-02-20 13:32:33 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 
+
 import mongoose from "mongoose";
-const dbConnection = async() => {
-    const DB_URI = "mongodb+srv://devanshu1336:Devanshu1229@job-board.pmd93qc.mongodb.net/?retryWrites=true&w=majority"
+const dbConnection = async(username,password) => {
+    const DB_URI = `mongodb+srv://${username}:${password}@job-board.pmd93qc.mongodb.net/?retryWrites=true&w=majority`
     try {
         await mongoose.connect(DB_URI, {useNewUrlParser: true});
         console.log("Connection Successfull");
