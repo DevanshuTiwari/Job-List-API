2b0800f33b8a456642bf523f036a835c5d334449
Devanshu Tiwari <devanshu1336@gmail.com>
2024-02-20 13:32:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import router from './routes/route.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dbConnection from './database/db.js';
 
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json({extended : true}));
@@ -12,5 +14,9 @@ app.use('/', router);
 
 const PORT = process.env.PORT || 8000;
 
-dbConnection();
+
+
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD
+dbConnection(USERNAME,PASSWORD);
